1dfe47dfe4becd6dc83840faf5371c849d654e36
LimitlessWS <Couqsell@gmail.com>
2025-11-04 15:54:17 +0100

    initial commit

--- js/AOS.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  AOS.init({
-    duration: 700, // 1 second fade duration
-    once: true,     // animations happen only once
-    offset: 100,    // start animation slightly before in view
-    easing: 'ease-in-out'
-  });
-});
